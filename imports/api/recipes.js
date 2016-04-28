b3a26f1a1a798b37fa3dec77c8957ecfc0cddf1b
VanHumbeeckA <drikkesband@hotmail.com>
2016-04-28 17:04:21 +0200

    recipe publish

--- imports/api/recipes.js
+++ imports/api/recipes.js
@@ -5,3 +5,14 @@ import {check} from 'meteor/check';
 // READ ONLY! no methods here!
 export const Recipes = new Mongo.Collection('recipes');
 
+if (Meteor.isServer) {
+    Meteor.publish('all-recipes', function() {
+        return Recipes.find({});
+    });
+
+    Meteor.publish('recipe', function(tag) {
+        check(tag, String);
+
+        return Recipes.find({name: { $regex: '*' + tag + '*'}});
+    });
+}
