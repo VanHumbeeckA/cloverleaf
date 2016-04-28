008276d0fe7d88bd88398d27029e22379c5bcf6b
Wouter Seyen <wouter.seyen@gmail.com>
2016-04-28 19:53:29 +0200

    my week

--- client/components/weekList/weekList.js
+++ client/components/weekList/weekList.js
@@ -20,7 +20,8 @@ class WeekListCtrl {
             var date = moment(today).add(i, 'days');
 
             this.week.push({
-                day: date.toDate()
+                day: date.toDate(),
+                nbOfEaters: 2
             });
         }
     }
