36f15aa0b2fa913603a69e0bf096dccfb2e2c576
Wouter Seyen <wouter.seyen@gmail.com>
2016-04-28 14:46:02 +0200

    intro angular-ui-router

--- client/main.js
+++ client/main.js
@@ -1,6 +1,7 @@
 import angular from 'angular';
 import angularMeteor from 'angular-meteor';
 import ngMaterial from 'angular-material';
+import uiRouter from 'angular-ui-router';
 import todosList from '../imports/components/todosList/todosList';
 import navigation from './components/navigation/navigation';
 import '../imports/startup/accounts-config';
@@ -8,10 +9,11 @@ import '../imports/startup/accounts-config';
 angular.module('cloverleaf', [
     angularMeteor,
     ngMaterial,
+    uiRouter,
     todosList.name,
     navigation.name,
     'accounts.ui'
-]);
+]).config(['$stateProvider', '$urlRouterProvider', ($stateProvider, $urlRouterProvider) => new RoutingConfig($stateProvider, $urlRouterProvider)]);
 
 function onReady() {
     angular.bootstrap(document, ['cloverleaf']);
@@ -21,4 +23,17 @@ if (Meteor.isCordova) {
     angular.element(document).on('deviceready', onReady);
 } else {
     angular.element(document).ready(onReady);
+}
+
+class RoutingConfig {
+
+    constructor($stateProvider, $urlRouterProvider) {
+
+        $urlRouterProvider.otherwise('/my-week');
+
+        $stateProvider.state('my-week', {
+            url: '/my-week',
+            templateUrl: 'client/views/my-week.html'
+        });
+    }
 }
