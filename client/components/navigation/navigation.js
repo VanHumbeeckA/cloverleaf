8df1adf1448a2fc743245a0c33d537aa307b27c3
MajorT1990 <wouter.moermans@gmail.com>
2016-04-28 16:07:28 +0200

    Loggin button in menu

--- client/components/navigation/navigation.js
+++ client/components/navigation/navigation.js
@@ -2,7 +2,40 @@ import angular from 'angular';
 
 import template from './navigation.html';
 
+class NavigationCtrl {
+    constructor($scope) {
+        $scope.viewModel(this);
+        this.username = "";
+        this.loggedIn = false;
+    }
+
+    loginWithGoogle(){
+        Meteor.loginWithGoogle({}, (err) => {
+            console.log("returned");
+            if (err) {
+                //Session.set('errorMessage', err.reason || 'Unknown error');
+                console.log(err);
+                this.loggedIn = false;
+            }
+            else {
+                console.log("true!!!")
+                this.loggedIn = true;
+                this.username = Meteor.user().profile.name;
+            }
+        });
+    }
+
+    logout(){
+        Meteor.logout( (err) => {
+            this.loggedIn = false;
+            if(err)console.error(err);
+        });
+    }
+
+}
+
 export default angular.module('navigation', [])
     .component('navigation', {
         templateUrl: 'client/components/navigation/navigation.html',
+        controller: ['$scope', NavigationCtrl]
     })
